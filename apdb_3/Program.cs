e2ef8cddc059606a2d7a3eacffad9e5db47f7bbb
kamehame_ha <bartek@kluz.pro>
2026-03-19 13:28:11 +0100

    Need to fix GetRecord so it does allow casting

--- apdb_3/Program.cs
+++ apdb_3/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using apdb_3.Classes;
+
+Console.WriteLine("Start");
+
+User testuser = new User
+{
+    Username = "Test",
+    PermissionLevel = 0,
+    Password = "Test"
+};
+
+Database.AddRecord("users", testuser);
+
+User testuser2 = new User
+{
+    Username = "Test2",
+    PermissionLevel = 1,
+    Password = "Test2"
+};
+
+Database.AddRecord("users", testuser2);
+
+//User targetuser = (User)Database.GetRecord("users", "Username", "Test");
+
+//Console.WriteLine($"Found user!\nUsername: {targetuser.Username}");
+
+Console.ReadLine();
+
+Database.DeleteRecord("users", testuser2);
+
+Console.WriteLine("Finish");
